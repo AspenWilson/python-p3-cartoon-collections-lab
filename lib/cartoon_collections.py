6a56a09458d5fbc8b8da9914bbefcffa9e72b7c5
Aspen Wilson <aspen.schell@gmail.com>
2023-05-14 14:06:01 -0700

    completed lab

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,21 @@
-def roll_call_dwarves():
-    pass
+def roll_call_dwarves(dwarves):
+    index= 1
+    for name in dwarves:
+        print(f"{index}. {name}")
+        index +=1
 
-def summon_captain_planet():
-    pass
+def summon_captain_planet(calls):
+    return [i.title()+"!" for i in calls]
 
-def long_planeteer_calls():
-    pass
 
-def find_the_cheese():
-    pass
+def long_planeteer_calls(calls):
+    for i in calls:
+        if len(i) > 4:
+            return True
+    return False
+
+def find_the_cheese(foods):
+    for i in foods:
+        if i == 'gouda' or i=='cheddar' or i=='camembert':
+            return i
+    return None
